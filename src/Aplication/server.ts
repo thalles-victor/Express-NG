2c324359d8870c5f8a1f97898b119587558296c7
thalles-victor <thalles.victor2012@gmail.com>
2022-11-14 17:09:53 -0300

    :sparkles: feat: user model created and docker compose file

--- src/Aplication/server.ts
+++ src/Aplication/server.ts
@@ -1,10 +1,12 @@
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
+
 import { PORT } from "./ENV"
-
-
+import { userRouter } from "./Adapter/RestAPI/userRouter";
 
 const app = express();
+
+app.use(userRouter);
 
 app.listen(
   PORT,
