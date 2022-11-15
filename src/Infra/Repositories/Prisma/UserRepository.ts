0cf24fac9679f7e293b0b16828bfe384be98a139
thalles-victor <thalles.victor2012@gmail.com>
2022-11-15 06:22:02 -0300

    :sparkles: feat: reletions model implemented

--- src/Infra/Repositories/Prisma/UserRepository.ts
+++ src/Infra/Repositories/Prisma/UserRepository.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, User } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { RegisterUserEntity } from "../../../Aplication/Adapter/User/Methods/Register/core/RegisterUser.Entity";
 import { UserGlobalRepresentation } from "../../../Aplication/Adapter/User/Methods/User.GlobalRepresentation";
 import { IUserRepositoryContract } from "../../core/IUserRepository.Contract";
